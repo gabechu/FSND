0e94c22511bbf101aaceb614dbd6c7707885dc4b
Gabe <wei.g.chu@gmail.com>
2020-03-26 23:35:08 +0800

    replace with FlaskForm

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,9 +1,12 @@
 from datetime import datetime
-from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
-from wtforms.validators import DataRequired, AnyOf, URL
 
-class ShowForm(Form):
+from flask_wtf import FlaskForm
+from wtforms import (DateTimeField, SelectField, SelectMultipleField,
+                     StringField)
+from wtforms.validators import URL, AnyOf, DataRequired
+
+
+class ShowForm(FlaskForm):
     artist_id = StringField(
         'artist_id'
     )
@@ -16,7 +19,7 @@ class ShowForm(Form):
         default= datetime.today()
     )
 
-class VenueForm(Form):
+class VenueForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -117,7 +120,7 @@ class VenueForm(Form):
         'facebook_link', validators=[URL()]
     )
 
-class ArtistForm(Form):
+class ArtistForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
