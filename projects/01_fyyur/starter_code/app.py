e60412321523eebbb186184ac0901ad508ebb366
Gabe <wei.g.chu@gmail.com>
2020-04-04 15:09:35 +0800

    update search venues

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -192,17 +192,17 @@ def venues():
 
 @app.route("/venues/search", methods=["POST"])
 def search_venues():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for Hop should return "The Musical Hop".
-    # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    search_term = request.form.get("search_term", "")
+    matches = Venue.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
+
     response = {
-        "count": 1,
-        "data": [{"id": 2, "name": "The Dueling Pianos Bar", "num_upcoming_shows": 0,}],
+        "count": len(matches),
+        "data": [{"id": item.id, "name": item.name} for item in matches],
     }
     return render_template(
         "pages/search_venues.html",
         results=response,
-        search_term=request.form.get("search_term", ""),
+        search_term=search_term,
     )
 
 
@@ -278,7 +278,8 @@ def search_artists():
     # search for "band" should return "The Wild Sax Band".
     response = {
         "count": 1,
-        "data": [{"id": 4, "name": "Guns N Petals", "num_upcoming_shows": 0,}],
+        "data": 
+        [{"id": 4, "name": "Guns N Petals", "num_upcoming_shows": 0,}],
     }
     return render_template(
         "pages/search_artists.html",
