8ba1da7d58406a905b6f5296dd0a65c661f04080
Gabe <wei.g.chu@gmail.com>
2020-04-04 15:11:43 +0800

    update search artists

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -214,7 +214,7 @@ def show_venue(venue_id):
 
     shows_dict = compose_show_attributes(venue_shows)
     venue_dict.update(shows_dict)
-    # TODO: fix GET /venues/None HTTP/1.0" 405
+    # FIXME: fix GET /venues/None HTTP/1.0" 405
     return render_template("pages/show_venue.html", venue=venue_dict)
 
 
@@ -273,18 +273,18 @@ def artists():
 
 @app.route("/artists/search", methods=["POST"])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get("search_term", "")
+    matches = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+
     response = {
-        "count": 1,
-        "data": 
-        [{"id": 4, "name": "Guns N Petals", "num_upcoming_shows": 0,}],
+        "count": len(matches),
+        "data": [{"id": item.id, "name": item.name} for item in matches],
     }
+
     return render_template(
         "pages/search_artists.html",
         results=response,
-        search_term=request.form.get("search_term", ""),
+        search_term=search_term,
     )
 
 
